80aa852e65e3744d41531b1b0a720bbe9355de8c
Yinka <asyinka@gmail.com>
2023-05-07 21:37:10 +0100

    msc: add group hover

--- src/component/SolnContent.jsx
+++ src/component/SolnContent.jsx
@@ -1,17 +1,10 @@
-import {
-  MdArrowBack,
-  MdArrowBackIosNew,
-  MdArrowForwardIos,
-  MdArrowRight,
-  MdArrowRightAlt,
-  MdSupervisedUserCircle,
-} from "react-icons/md";
+import { MdArrowForwardIos, MdSupervisedUserCircle } from "react-icons/md";
 
 const SolutionsContent = () => {
   return (
     <div className="w-60 shadow p-3">
-      <div className="flex hover:bg-gray-200 cursor-pointer py-2">
-        <div className="mx-2 bg-gray-200">
+      <div className="flex hover:bg-gray-100 group cursor-pointer py-2">
+        <div className="mx-2 group-hover:text-blue-400 bg-gray-100">
           <MdSupervisedUserCircle size={"40px"} />
         </div>
         <div>
@@ -22,8 +15,8 @@ const SolutionsContent = () => {
         </div>
       </div>
 
-      <div className="flex hover:bg-gray-200 cursor-pointer py-2">
-        <div className="mx-2 bg-gray-200">
+      <div className="flex group hover:bg-gray-100 hover:first-child:text-blue-400 cursor-pointer py-2">
+        <div className="mx-2 group-hover:text-blue-400 bg-gray-100">
           <MdArrowForwardIos size={"40px"} />
         </div>
         <div>
