847cbe66ac7af78cdf3ded131531aeb0b9d8e703
Yinka <asyinka@gmail.com>
2023-05-07 02:03:51 +0100

    add navBar as component

---
+++ src/component/navBar.jsx
@@ -0,0 +1,10 @@
+const navBar = () => {
+  return (
+    <div className="navBarContainer">
+      <div className="navBar__heading">educative</div>
+      <div className="navBar__"></div>
+    </div>
+  );
+};
+
+export default navBar;
