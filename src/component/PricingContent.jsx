ad01b1d72d32cf21ecbbc418ba9d52e888119635
Yinka <asyinka@gmail.com>
2023-05-08 00:31:49 +0100

    add solution content component

--- src/component/PricingContent.jsx
+++ src/component/PricingContent.jsx
@@ -1,5 +1,20 @@
-const PricingContent = () => {
-  return <div></div>;
+import { MdSupervisedUserCircle } from "react-icons/md";
+
+const PricingContent = ({ childrenH2, childrenP, iconName }) => {
+  return (
+    <div className="shadow w-auto inline-block">
+      <HoverContent
+        childrenH2={"For Enterprise"}
+        childrenP={"Tailored for your team"}
+        iconName={<MdSupervisedUserCircle size={"40px"} />}
+      />
+      <HoverContent
+        childrenH2={"For indivi"}
+        childrenP={"Learn new technologies"}
+        iconName={<MdArrowForwardIos size={"40px"} />}
+      />
+    </div>
+  );
 };
 
 export default PricingContent;
